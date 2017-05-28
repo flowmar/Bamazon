2c18eab2c312f6ccb1ea51e68ed3869f63409e4d
Flowmar <omar.imam@kineticvortex.com>
2017-05-28 14:32:56 -0500

    Add Javascript to connect to Bamazon database via node

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,24 @@
+// Require mysql package
 var mysql = require('mysql');
 
+// Create connection variable
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'products'
+    // Hostname and port
+    host: "localhost",
+    port: 3306,
+
+    // Username
+    user: "root",
+    password: "",
+    database: "Bamazon"
 });
 
-connection.connect((function(err) {
-    if (err)
-}))
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to Bamazon as " + connection.threadId);
+});
+
+connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+});
